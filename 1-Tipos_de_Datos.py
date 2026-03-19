147a8c7a41b897468bf018f02161d86a74887983
Agustin Addur <addurlucas@gmail.com>
2026-03-19 18:11:43 -0300

    agregue u entero 5

--- 1-Tipos_de_Datos.py
+++ 1-Tipos_de_Datos.py
@@ -1,6 +1,8 @@
 enteros1 = 1
 enteros2 = 2
 enteros3 = -4
+enteros4 = 0
+enteros5 = 1_000_000
 
-print(type(enteros1), type(enteros2), type(enteros3))
-print(enteros1, enteros2, enteros3)
+print(type(enteros1), type(enteros2), type(enteros3), type(enteros4), type(enteros5))
+print(enteros1, enteros2, enteros3, enteros4, enteros5)
